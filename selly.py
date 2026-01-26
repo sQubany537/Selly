7b6a62d3d4b9bd63f851b1bffd82d2e65fc1deb4
sQubany537 <wiktorczerech58@gmail.com>
2026-01-26 18:46:15 +0100

    Update selly.py

--- selly.py
+++ selly.py
@@ -1,1 +1,26 @@
+import streamlit as st
 
+# 1. Konfiguracja strony
+st.set_page_config(page_title="Hey Selly App", layout="wide")
+
+# 2. Odrobina CSS, aby wycentrować przycisk w pionie i poziomie
+st.markdown("""
+    <style>
+    .stButton {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80vh; /* Centrowanie w pionie na 80% wysokości ekranu */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# 3. Tworzenie kolumn dla centrowania w poziomie
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    # Wyświetlenie przycisku
+    if st.button("Hey Selly"):
+        # Akcja po kliknięciu
+        st.balloons()
+        st.markdown("<h1 style='text-align: center;'>Cześć Selly! ✨</h1>", unsafe_allow_html=True)
